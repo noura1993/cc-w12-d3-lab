77b17e1e77f3f4c9d7616e603fef33bb8c2eac69
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-29 15:38:22 +0100

    Add tests for HybirdCar

--- Cars/src/main/java/HybirdCar.java
+++ Cars/src/main/java/HybirdCar.java
@@ -1,2 +1,23 @@
-public class HybirdCar {
+public class HybirdCar extends Vehicle{
+
+    private Engine engine;
+    private ElectricMotor electricMotor;
+
+    public HybirdCar(double price, String color, Engine engine, ElectricMotor electricMotor) {
+        super(price, color);
+        this.engine = engine;
+        this.electricMotor = electricMotor;
+    }
+
+    public double getEngineSize() {
+        return this.engine.getSize();
+    }
+
+    public int getEngineFullTank() {
+        return this.engine.getFullTank();
+    }
+
+    public double getBattery() {
+        return this.electricMotor.getBattery();
+    }
 }
--- Cars/src/test/java/HybirdCarTest.java
+++ Cars/src/test/java/HybirdCarTest.java
@@ -1,2 +1,43 @@
-//public class HybirdCarTest extends Vehicle{
-//}
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HybirdCarTest {
+    
+    ElectricMotor electricMotor;
+    Engine engine;
+    HybirdCar hybirdCar;
+
+    @Before
+    public void before(){
+        electricMotor = new ElectricMotor(1000);
+        engine = new Engine(100, 3.5);
+        hybirdCar = new HybirdCar(5000, "Red", engine, electricMotor);
+    }
+
+    @Test
+    public void getPrice() {
+        assertEquals(5000, hybirdCar.getPrice(), 0.001);
+    }
+
+    @Test
+    public void getColor() {
+        assertEquals("Red", hybirdCar.getColor());
+    }
+
+    @Test
+    public void getEngineSize() {
+        assertEquals(3.5, hybirdCar.getEngineSize(), 0.001);
+    }
+
+    @Test
+    public void getEngineFullTank() {
+        assertEquals(100, hybirdCar.getEngineFullTank());
+    }
+
+    @Test
+    public void getBattery() {
+        assertEquals(1000, this.hybirdCar.getBattery(),0.001);
+    }
+}
