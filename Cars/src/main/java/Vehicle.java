50fcbb06fb088b2e84323678282738bdce50e6ba
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-07-29 14:13:09 +0100

    Implement Vehicle class

---
+++ Cars/src/main/java/Vehicle.java
@@ -0,0 +1,18 @@
+public abstract class Vehicle {
+
+    private double price;
+    private String color;
+
+    public Vehicle(double price, String color) {
+        this.price = price;
+        this.color = color;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
